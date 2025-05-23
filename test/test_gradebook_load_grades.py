cf2c2f8728898234ac4c5c9d899a2bb5e803eb29
Peter Aronoff <peter@aronoff.org>
2025-05-23 08:17:47 -0400

    chore: use fixtures to test load_grades

--- test/test_gradebook_load_grades.py
+++ test/test_gradebook_load_grades.py
@@ -2,18 +2,33 @@
 
 from pathlib import Path
 
+import pytest
+
 from gradebook import gradebook_calc as gbc
 from gradebook import gradebook_student as gbs
 
 
-def test_load_grades_null_vs_zero():
-    john = gbs.Student("John", "Doe", "john.doe@school.edu", ["major"])
-    jane = gbs.Student("Jane", "Doe", "jane.doe@school.edu", ["major"])
-    student_objs = {john.email: john, jane.email: jane}
-    gbc.load_grades(student_objs, Path.cwd() / "test/data")
+# pylint: disable=redefined-outer-name
+@pytest.fixture
+def john():
+    return gbs.Student("John", "Doe", "john.doe@school.edu", ["major"])
 
-    assert john.has_grades("major") == True
-    assert jane.has_grades("major") == False
+
+@pytest.fixture
+def jane():
+    return gbs.Student("Jane", "Doe", "jane.doe@school.edu", ["major"])
+
+
+@pytest.fixture
+def student_pair(john, jane):
+    return {john.email: john, jane.email: jane}
+
+
+def test_load_grades_null_vs_zero(student_pair, john, jane):
+    gbc.load_grades(student_pair, Path.cwd() / "test/data")
+
+    assert john.has_grades("major") is True
+    assert jane.has_grades("major") is False
 
 
 def test_load_grades_catches_key_error():
